960c76700c1d5c0ce26dcd8b9aedb41c1daa0568
ʀᴜɴᴛɪᴍᴇ ( Dʀᴀɢᴏsʜ ) <39906742+0dragosh@users.noreply.github.com>
2025-04-03 10:58:47 +0300

    feat: add additional business info & extra bank account (#32)
    * feat: add additional business info & extra bank account

--- internal/models/business.go
+++ internal/models/business.go
@@ -2,17 +2,23 @@ package models
 
 // Business represents the consultant's business details
 type Business struct {
-	ID          int    `json:"id"`
-	Name        string `json:"name"`
-	Address     string `json:"address"`
-	City        string `json:"city"`
-	PostalCode  string `json:"postal_code"`
-	Country     string `json:"country"`
-	VatID       string `json:"vat_id"`
-	Email       string `json:"email"`
-	BankName    string `json:"bank_name"`
-	BankAccount string `json:"bank_account"`
-	IBAN        string `json:"iban"`
-	BIC         string `json:"bic"`
-	LogoPath    string `json:"logo_path"`
+	ID                  int    `json:"id"`
+	Name                string `json:"name"`
+	Address             string `json:"address"`
+	City                string `json:"city"`
+	PostalCode          string `json:"postal_code"`
+	Country             string `json:"country"`
+	VatID               string `json:"vat_id"`
+	Email               string `json:"email"`
+	BankName            string `json:"bank_name"`
+	BankAccount         string `json:"bank_account"`
+	IBAN                string `json:"iban"`
+	BIC                 string `json:"bic"`
+	Currency            string `json:"currency"`
+	SecondBankName      string `json:"second_bank_name"`
+	SecondIBAN          string `json:"second_iban"`
+	SecondBIC           string `json:"second_bic"`
+	SecondCurrency      string `json:"second_currency"`
+	ExtraBusinessDetail string `json:"extra_business_detail"`
+	LogoPath            string `json:"logo_path"`
 }
